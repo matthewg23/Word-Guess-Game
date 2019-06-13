3fdf8167a0450837557d0811a866c1519f90a427
Matthew <mattg1827@gmail.com>
2019-06-12 19:46:35 -0700

    Added images, styles, and js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,38 +1,44 @@
 // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
-var computerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var wordChoices = ["Eagle", "Tiger", "Rhinoceros", "Monkey", "Giraffe", "Elephant"];
 
 // Creating variables to hold the number of wins, losses, and ties. They start at 0.
 var wins = 0;
 var losses = 0;
 var userGuesses = [];
-var guessesLeft = 9;
 
 // Create variables that hold references to the places in the HTML where we want to display things.
-var userChoiceText = document.getElementById("guessesSoFarText");
-var lossesText = document.getElementById("guessesLeftText");
+var hi = document.getElementById("winsText");
+document.getElementById("currentWordBlank");
+var guessesLeft = document.getElementById("guessesLeft");
+var userGuesses = document.getElementById("userGuesses");
 
 // This function is run whenever the user presses a key.
 document.onkeyup = function(event) {
 
+    var currentWord = wordChoices[Math.floor(Math.random() * wordChoices.length)];
     var userGuess = event.key;
-    guessesLeft--;
-    document.getElementById("guessesLeftText").innerHTML = "Guesses left: " + guessesLeft;
-    userGuesses.push(userGuess);
-    document.getElementById("guessesSoFarText").innerHTML = "Guesses so far: " + userGuesses;
+    
+    
 
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+    // Print the letters of the chosen word as blank spaces to fill in 
+    var lengthofword = currentWord.length;
+    var blankspace = ' _ ';
 
-    if ( userGuess == computerGuess) {
-      wins++;
-      document.getElementById("winsText").innerHTML = "Wins: " + wins;
-    }
-
-    if (guessesLeft == "0") {
-      losses++;
-      document.getElementById("lossesText").innerHTML = "Losses: " + losses;
-      userGuesses = [];
-      guessesLeft = 9;
-    }
+    document.getElementById("currentWordBlank").innerHTML = blankspace.repeat(lengthofword);
+    // document.getElementById("guessesLeft").innerHTML = guessesLeft;
 
   }; 
   
+
+
+
+  // "winsText"> </span>
+  //               </p>
+
+  //               <p>Current word: <span id="currentWord">  </span>
+  //               </p>
+
+  //               <p>Number of Guesses Remaining: <span id="guessesLeft"> </span>
+  //               </p>
+
+  //               <p>Letters Already Guessed: <span id="userGuesses"></span>
